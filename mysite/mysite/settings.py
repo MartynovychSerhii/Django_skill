bc46e0a7133a5f99a12c5c8a5f604f63b0898ec5
MartynovychSerhii <131612238+MartynovychSerhii@users.noreply.github.com>
2026-03-09 20:36:34 +0200

    new changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
+    "shopapp.apps.ShopappConfig",
 ]
 
 MIDDLEWARE = [
---
+++ mysite/shopapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+# Create your views here.
+
+def shop_index(request: HttpRequest) -> HttpResponse:
+    print(request.path)
+    print(request.method)
+    print(request.headers)
+    return HttpResponse("<h1>Hello World</h1>")
