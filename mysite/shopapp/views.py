8f53cf09b5389bcfda6924e081b9e3630e0ca030
MartynovychSerhii <131612238+MartynovychSerhii@users.noreply.github.com>
2026-03-24 15:18:30 +0200

    added groups list template

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,10 +1,26 @@
+from timeit import default_timer as timer
+
+from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def shop_index(request: HttpRequest) -> HttpResponse:
-    print(request.path)
-    print(request.method)
-    print(request.headers)
-    return HttpResponse("<h1>Hello World</h1>")
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 3999)
+    ]
+    context = {
+        "time_running": timer(),
+        "products": products,
+    }
+    return render(request, "shopapp/shop-index.html", context=context)
+
+def groups_list(request: HttpRequest) -> HttpResponse:
+    context = {
+        "groups": Group.objects.prefetch_related('permissions').all(),
+    }
+    return render(request, "shopapp/groups-list.html", context=context)
